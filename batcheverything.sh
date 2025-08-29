2024e3f604f6529bcc1d1f3fcd575b9cf198812e
John W Carlson <yottzumm@gmail.com>
2025-08-29 17:44:56 -0500

    Final checkin for the day.  Things seem to work

--- batcheverything.sh
+++ batcheverything.sh
@@ -1,3 +1,4 @@
+echo "Downloading Walking Alien.gltf..."
 curl https://create3000.github.io/media/glTF/Walking%20Alien/textures/InnerMouth_baseColor.jpeg --output textures/InnerMouth_baseColor.jpeg 
 curl https://create3000.github.io/media/glTF/Walking%20Alien/textures/EyeSockets_baseColor.jpeg --output textures/EyeSockets_baseColor.jpeg 
 curl https://create3000.github.io/media/glTF/Walking%20Alien/textures/SkinHead_baseColor.jpeg --output textures/SkinHead_baseColor.jpeg 
@@ -7,14 +8,23 @@ curl https://create3000.github.io/media/glTF/Walking%20Alien/textures/SkinBody_b
 curl https://create3000.github.io/media/glTF/Walking%20Alien/scene.bin --output scene.bin 
 curl https://create3000.github.io/media/glTF/Walking%20Alien/Walking%20Alien.gltf --output WalkingAlien.gltf 
 
+echo "Converting Walking Alien to .x3d and .x3dv..."
 npx x3d-tidy@latest -f 5 -d 5 -i WalkingAlien.gltf -o WalkingAlienX_ITE.x3d
 npx x3d-tidy@latest -f 5 -d 5 -i WalkingAlien.gltf -o WalkingAlienX_ITE.x3dv
+
+echo "Mapping joints and interpolators..."
 python runwalkingalienX_ITE.py
+python runwalkingman.py
+echo "Converting Walking Alien Final Output from .x3d to .x3dv..."
 npx x3d-tidy@latest -i WalkingAlienFinalOutputX_ITE.x3d -o WalkingAlienFinalOutputX_ITE.x3dv
+echo "Touchups before finding validating..."
 (echo '%s/PROFILE Interchange/PROFILE Immersive/'; echo g/colorSpaceConversion/d;  echo wq;) > ex1.cmds
 ex WalkingAlienFinalOutputX_ITE.x3dv < ex1.cmds
+echo "Adding animations..."
 cat Animations.x3dv >> WalkingAlienFinalOutputX_ITE.x3dv
+echo "Validating..."
 ~/Downloads/castle-model-viewer-5.3.0-win64-x86_64/castle-model-viewer/castle-model-converter.exe --validate WalkingAlienFinalOutputX_ITE.x3dv
+echo "Commenting out excess ROUTES (no joints present)..."
 (~/Downloads/castle-model-viewer-5.3.0-win64-x86_64/castle-model-viewer/castle-model-converter.exe --validate WalkingAlienFinalOutputX_ITE.x3dv 2>&1 |grep "not found"|sed 's/" not found//'| sed 's/.*Route destination node name "//'|sort -u| sed 's/\(.*\)/%s\/^\\(.*\1.*\\)\/# \\1\//'; echo wq) > ex2.cmds
 ex WalkingAlienFinalOutputX_ITE.x3dv < ex2.cmds
 npx sunrize@latest WalkingAlienFinalOutputX_ITE.x3dv
--- runwalkingman.py
+++ runwalkingman.py
@@ -5,9 +5,9 @@ from rename_interpolators import rename_interpolators
 INPUT_PREFIX = "CC_"
 INPUT_FILE = "walking_man_cc_test.new.python.x3d"
 OUTPUT_PREFIX = "John_"
-OUTPUT_FILE = "joints_mapped.x3d"
+OUTPUT_FILE = "walking_man_cc_test_joints_mapped.x3d"
 map_joints(INPUT_PREFIX, INPUT_FILE, skeleton_map_list, OUTPUT_PREFIX, OUTPUT_FILE)
 
-FINAL_FILE = "interpolators_mapped.x3d"
+FINAL_FILE = "walking_man_cc_test_interpolators_mapped.x3d"
 TIME_SENSORS = [ "WalkTimer" ]
 rename_interpolators(OUTPUT_FILE, TIME_SENSORS, FINAL_FILE)
