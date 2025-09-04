09dba0ee0532a449092757e55474c38df1414354
John W Carlson <yottzumm@gmail.com>
2025-09-04 13:42:24 -0500

    Reworked interfaces, will get to class eventually

--- joint_binding_remove.py
+++ joint_binding_remove.py
@@ -5,10 +5,7 @@ import glob
 import sys
 import time
 
-def joint_binding_remove(INPUT_FILE, OUTPUT_FILE):
-
-    X3D = xml.etree.ElementTree.parse(INPUT_FILE)
-    root = X3D.getroot()
+def joint_binding_remove(root):
 
     humanoids = root.findall(".//HAnimHumanoid")
     for humanoid in humanoids:
@@ -18,11 +15,3 @@ def joint_binding_remove(INPUT_FILE, OUTPUT_FILE):
         #del humanoid.attrib["jointBindingPositions"]
         #del humanoid.attrib["jointBindingRotations"]
         #del humanoid.attrib["jointBindingScales"]
-
-    header = '<?xml version="1.0" encoding="UTF-8"?>\n<!DOCTYPE X3D PUBLIC "ISO//Web3D//DTD X3D 4.0//EN" "https://www.web3d.org/specifications/x3d-4.0.dtd">'
-    xmlstr = xml.etree.ElementTree.tostring(root, encoding='unicode')
-
-    xmlString = f"{header}{xmlstr}"
-    file_output = os.path.join("./",os.path.basename(OUTPUT_FILE))
-    with open(file_output, "w") as output_file:
-        output_file.write(xmlString)
