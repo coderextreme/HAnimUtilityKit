c64faa6ac26da70cb959b218cd71780f19c60efc
John W Carlson <yottzumm@gmail.com>
2025-09-13 16:20:42 -0500

    commented out body snatching code

--- HAnimUtility.py
+++ HAnimUtility.py
@@ -166,7 +166,7 @@ class HAnimUtility:
     def standard_rename(self, INPUT_FILE, skeleton_map_list, INPUT_PREFIX, OUTPUT_PREFIX, TIME_SENSORS, FINAL_FILE):
         self.readXML(INPUT_FILE)
         self.animation_remove()
-        self.humanoid_replace()
+        # self.humanoid_replace()
         self.map_joints(skeleton_map_list, INPUT_PREFIX, OUTPUT_PREFIX)
         # We removed animations, above.
         # self.rename_interpolators(TIME_SENSORS)
@@ -183,7 +183,7 @@ class HAnimUtility:
     def standard_rename_with_group(self, INPUT_FILE, skeleton_map_list, INPUT_PREFIX, OUTPUT_PREFIX, TIME_SENSORS, FINAL_FILE):
         self.readXML(INPUT_FILE)
         self.animation_remove()
-        self.humanoid_replace()
+        # self.humanoid_replace()
         self.group_remove()
         self.map_joints(skeleton_map_list, INPUT_PREFIX, OUTPUT_PREFIX)
         # We removed animations, above.
@@ -194,7 +194,7 @@ class HAnimUtility:
     def interpolator_rename(self, INPUT_FILE, skeleton_map_list, INPUT_PREFIX, OUTPUT_PREFIX, TIME_SENSORS, FINAL_FILE):
         self.readXML(INPUT_FILE)
         # animations are not removed
-        self.humanoid_replace()
+        # self.humanoid_replace()
         self.map_joints(skeleton_map_list, INPUT_PREFIX, OUTPUT_PREFIX)
         self.rename_interpolators(TIME_SENSORS)
         self.writeXML(FINAL_FILE)
@@ -204,7 +204,7 @@ class HAnimUtility:
         self.readXML(INPUT_FILE)
         # animations were removed by hand
         # self.animation_remove()
-        self.humanoid_replace()
+        # self.humanoid_replace()
         self.map_joints(skeleton_map_list, INPUT_PREFIX, OUTPUT_PREFIX)
         # animations were removed by hand
         # self.rename_interpolators(root, TIME_SENSORS)
@@ -217,7 +217,7 @@ class HAnimUtility:
     def tidy_rename(self, INPUT_FILE, skeleton_map_list, INPUT_PREFIX, OUTPUT_PREFIX, TIME_SENSORS, FINAL_FILE):
         self.readXML(INPUT_FILE)
         self.animation_remove()
-        self.humanoid_replace()
+        # self.humanoid_replace()
         self.map_joints(skeleton_map_list, INPUT_PREFIX, OUTPUT_PREFIX)
         # We removed animations, above.
         # self.rename_interpolators(root, TIME_SENSORS)
