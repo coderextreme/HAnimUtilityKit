02f422753523eb41c1b06d7d4647be7484bab960
John W Carlson <yottzumm@gmail.com>
2025-11-21 10:11:46 -0600

    Checked in rigid stuff

--- HAnimToRigid.py
+++ HAnimToRigid.py
@@ -37,17 +37,39 @@ class HAnimToRigid:
                     pass
 
     def replace_segments(self):
-        for segment_parent in self.root.findall('.//HAnimSegment/..'):
-            for segment in segment_parent.findall('./HAnimSegment'):
+        segments = self.root.findall('.//HAnimSegment')
+        for segment in segments:
                 segment.tag = 'RigidBody'
-                if segment.get("containerField") == "segments":
-                    segment.set("containerField", "bodies")
-                else:
-                    segment.set("containerField", "body1")  # There's only one body per joint, more later
-                try:
-                    del segment.attrib["name"]
-                except KeyError:
-                    pass
+        parents = self.root.findall('.//HAnimJoint/..')
+        for parent_joint in parents:
+            #xmlstr = xml.etree.ElementTree.tostring(parent_joint, encoding='unicode')
+            #print(f"{xmlstr}")
+            parent_segments = parent_joint.findall('RigidBody')
+            for parent_segment in parent_segments:  # there should be only one
+                joints = parent_joint.findall('HAnimJoint')
+                for joint in joints:
+                    segments = joint.findall('RigidBody')  # one segment per joint
+                    for segment in segments:
+                        if parent_segment is not None and parent_segment.get("DEF") is not None:
+                            joint.set("body1", parent_segment.get("DEF"))
+                            # parent_segment.set("containerField", "body1")
+                            print(f"found parent segment {parent_segment.get('DEF')} def for {segment.get('DEF')}")
+                        elif parent_segment is None:
+                            print(f"no parent segment")
+                        else:
+                            print(f"found parent segment, DEF is None")
+                        if segment.get("DEF") is not None:
+                            joint.set("body2", segment.get("DEF"))
+                            # segment.set("containerField", "body2")
+                        try:
+                            del segment.attrib["name"]
+                        except KeyError:
+                            pass
+                        if joint.get('body1') is None:
+                            try:
+                                del joint.attrib["body2"]  # remove body2 if body1 isn't present
+                            except KeyError:
+                                pass
 
     def replace_joints(self):
         for joint_parent in self.root.findall('.//HAnimJoint/..'):
@@ -57,8 +79,7 @@ class HAnimToRigid:
                     del joint.attrib["name"]
                 except KeyError:
                     pass
-                # think about how a joint is in a rigid body collection
-                joint.set("containerField", "children")
+                # joint.set("containerField", "joints")
                 try:
                     if joint.attrib["center"]:
                         joint.set("anchorPoint", joint.attrib["center"])
@@ -106,9 +127,35 @@ class HAnimToRigid:
                 route.set("toField", "set_position")
                 route.set("toNode", body.attrib["DEF"])  # this is the rigid body, but there should only be one TODO
 
+    def rearrange_hierarchy(self):
+            humanoid = self.root.find('.//RigidBodyCollection')
+            segments = self.root.findall(".//RigidBody")
+            for segment in segments:
+                segment.set("containerField", "bodies")
+                parent = self.find_parent(segment)
+                parent.remove(segment)
+                humanoid.append(segment)
+
+            joints = self.root.findall(".//UniversalJoint")
+            for joint in joints:
+                joint.set("containerField", "joints")
+                for child in list(joint):
+                    humanoid.append(child)
+                    joint.remove(child)
+
+    def cleanup(self):
+        humanoids = self.root.findall('.//RigidBodyCollection')
+        for humanoid in humanoids:
+            segments = humanoid.findall(".//RigidBody[@USE]")
+            for segment in segments:
+                humanoid.remove(segment)
+            joints = humanoid.findall(".//UniversalJoint[@USE]")
+            for joint in joints:
+                humanoid.remove(joint)
+
+
     # writeXML.py
     def writeXML(self, OUTPUT_FILE):
-        # TODO remove to use Animations3.x3dv
         header = '<?xml version="1.0" encoding="UTF-8"?>\n<!DOCTYPE X3D PUBLIC "ISO//Web3D//DTD X3D 4.0//EN" "https://www.web3d.org/specifications/x3d-4.0.dtd">'
         xmlstr = xml.etree.ElementTree.tostring(self.root, encoding='unicode')
         xmlString = f"{header}{xmlstr}"
@@ -124,4 +171,6 @@ class HAnimToRigid:
         self.replace_joints()
         self.add_collidable_shapes()
         self.replace_route_fields()
+        self.rearrange_hierarchy()
+        self.cleanup()
         self.writeXML(OUTPUT_FILE)
---
+++ runrigid.py
@@ -0,0 +1,7 @@
+from HAnimToRigid import HAnimToRigid
+
+INPUT_FILE = "JinLOA4.scaled1.x3d"
+OUTPUT_FILE = "RigidJin.x3d"
+
+joedemo5 = HAnimToRigid()
+joedemo5.convertDemo5a(INPUT_FILE, OUTPUT_FILE)
